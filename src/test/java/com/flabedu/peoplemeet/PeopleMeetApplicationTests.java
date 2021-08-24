3bd90b28b45cc0603fb49aef5698fae905b5ccea
right42 <chjm452@naver.com>
2021-08-24 20:56:42 +0900

    project init

---
+++ src/test/java/com/flabedu/peoplemeet/PeopleMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flabedu.peoplemeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
