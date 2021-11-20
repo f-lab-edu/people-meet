52d9d4be75e82ea04c5ca646a94f34a1e068f11e
right42 <chjm452@naver.com>
2021-11-20 13:52:08 +0900

    공통 어플리케이션 exception 및 exception handler 추가

---
+++ src/main/java/com/flabedu/peoplemeet/exception/ApplicationException.java
@@ -0,0 +1,12 @@
+package com.flabedu.peoplemeet.exception;
+
+public class ApplicationException extends RuntimeException {
+
+	public ApplicationException() {
+	}
+
+	public ApplicationException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/com/flabedu/peoplemeet/web/common/GlobalController.java
@@ -0,0 +1,33 @@
+package com.flabedu.peoplemeet.web.common;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.flabedu.peoplemeet.exception.ApplicationException;
+
+import lombok.Getter;
+
+@RestControllerAdvice
+public class GlobalController {
+
+	@ExceptionHandler(ApplicationException.class)
+	public ResponseEntity<Error> applicationExceptionHandler(ApplicationException exception) {
+		return ResponseEntity.badRequest().body(Error.from(exception));
+	}
+
+	@Getter
+	static class Error {
+
+		private String message;
+
+		public static Error from(ApplicationException exception) {
+			Error error = new Error();
+
+			error.message = exception.getMessage();
+
+			return error;
+		}
+	}
+
+}
