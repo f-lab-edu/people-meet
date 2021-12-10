aa1848136b24e30ae1fa3e8e82c7bdd2dbd1174e
right42 <chjm452@naver.com>
2021-12-10 23:02:32 +0900

    table charset DDL 파일 추가

---
+++ src/main/resources/data/init-DDL.sql
@@ -0,0 +1,67 @@
+ALTER TABLE interest_category
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE interest
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE region
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE board
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE comments
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE group_interest
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE group_user
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE `groups`
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE meeting
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE meeting_join
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE notification
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE payment
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE payment_method
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE payment_method_card
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE role
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE user
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
+
+ALTER TABLE user_interest
+    COLLATE='utf8mb4_general_ci',
+    CONVERT TO CHARSET utf8mb4;
--- src/main/resources/data/interest-init.sql
+++ src/main/resources/data/interest-init.sql
@@ -1,4 +1,4 @@
-# INSERT INTO interest_category(interest_category_id, create_time, update_time, name) VALUES (1, now(), now(), 'IT');
+INSERT INTO interest_category(interest_category_id, create_time, update_time, name) VALUES (1, now(), now(), 'IT');
 INSERT INTO interest_category(interest_category_id, create_time, update_time, name) VALUES (2, now(), now(), '문화');
 INSERT INTO interest_category(interest_category_id, create_time, update_time, name) VALUES (3, now(), now(), '기타');
 
