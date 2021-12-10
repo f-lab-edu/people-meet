bb46763d84364b1cb54aa0dff86a32974fa89e78
right42 <chjm452@naver.com>
2021-12-11 00:14:18 +0900

    feat: Ehcache 추가, Region 캐시저장

---
+++ src/main/java/com/flabedu/peoplemeet/common/CacheInitLoader.java
@@ -0,0 +1,34 @@
+package com.flabedu.peoplemeet.common;
+
+import java.util.Objects;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Component;
+
+import com.flabedu.peoplemeet.domain.region.dto.RegionCacheDto;
+import com.flabedu.peoplemeet.domain.region.repository.RegionMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CacheInitLoader {
+
+	private final CacheManager cacheManager;
+
+	private final RegionMapper regionMapper;
+
+	@PostConstruct
+	public void init(){
+		regionCacheLoad();
+	}
+
+	private void regionCacheLoad() {
+		for (RegionCacheDto regionCacheDto : regionMapper.findAll()) {
+			Objects.requireNonNull(cacheManager.getCache("regions")).put(regionCacheDto.getRegionId(), regionCacheDto);
+		}
+	}
+
+}
