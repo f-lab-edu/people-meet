bdbccfef4cd430909c70359ccf7c5ff6cac6c817
right42 <chjm452@naver.com>
2022-01-01 02:13:25 +0900

    CacheService 메소드들 static으로 변경

--- src/main/java/com/flabedu/peoplemeet/common/CacheService.java
+++ src/main/java/com/flabedu/peoplemeet/common/CacheService.java
@@ -5,30 +5,31 @@ import java.util.Objects;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 
-import com.flabedu.peoplemeet.domain.interest.dto.InterestDto;
-import com.flabedu.peoplemeet.domain.region.dto.RegionCacheDto;
-
-import lombok.RequiredArgsConstructor;
+import com.flabedu.peoplemeet.dto.interest.InterestDto;
+import com.flabedu.peoplemeet.dto.region.RegionCacheDto;
 
 @Service
-@RequiredArgsConstructor
 public class CacheService {
 
 	private static final String REGION_CACHE_KEY = "regions";
 
 	private static final String INTEREST_CACHE_KEY = "interests";
 
-	private final CacheManager cacheManager;
+	private static CacheManager cacheManager;
 
-	public RegionCacheDto getRegion(final Long regionId) {
+	CacheService(CacheManager cacheManager) {
+		CacheService.cacheManager = cacheManager;
+	}
+
+	public static RegionCacheDto getRegion(final Long regionId) {
 		return getCache(REGION_CACHE_KEY, regionId, RegionCacheDto.class);
 	}
 
-	public InterestDto getInterest(final Long interestId) {
+	public static InterestDto getInterest(final Long interestId) {
 		return getCache(INTEREST_CACHE_KEY, interestId, InterestDto.class);
 	}
 
-	private <T> T getCache(final String cacheName, final Long key, Class<T> clazz) {
+	private static <T> T getCache(final String cacheName, final Long key, Class<T> clazz) {
 		return Objects.requireNonNull(cacheManager.getCache(cacheName)).get(key, clazz);
 	}
 }
